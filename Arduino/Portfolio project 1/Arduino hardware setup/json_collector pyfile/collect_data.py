dc54eb499e5fed9bc8db00f2fec17a7ab29b33e0
sierra-95 <michaelmachohi@gmail.com>
2023-10-04 00:39:59 +0300

    message

--- Arduino/Portfolio project 1/Arduino hardware setup/json_collector pyfile/collect_data.py
+++ Arduino/Portfolio project 1/Arduino hardware setup/json_collector pyfile/collect_data.py
@@ -4,9 +4,13 @@ import json
 # Define the serial port and baud rate
 ser = serial.Serial('com7', 9600)
 
-# Create a function to save data to a file
-def save_data(data):
+# Create functions to save data to the respective files
+def save_dht11_data(data):
     with open('data.txt', 'a') as file:
+        file.write(data + '\n')
+
+def save_rfid_data(data):
+    with open('login.txt', 'a') as file:
         file.write(data + '\n')
 
 while True:
@@ -24,8 +28,15 @@ while True:
         # Print the received JSON data for verification
         print("Received JSON data:", json_data)
 
-        # Save the JSON data to the file
-        save_data(line)
+        # Check if the JSON data has 'Humidity' key (DHT11 data)
+        if 'Humidity' in json_data:
+            # Save DHT11 data to 'data.txt'
+            save_dht11_data(line)
+
+        # Check if the JSON data has 'UID' key (RFID data)
+        if 'UID' in json_data:
+            # Save UID data to 'logic.txt'
+            save_rfid_data(line)
 
     except KeyboardInterrupt:
         # Exit the loop if Ctrl+C is pressed
